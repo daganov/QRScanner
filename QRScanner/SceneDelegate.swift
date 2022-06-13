ec31dfd20a1b93b571dc86f200312b81c973f783
Denis Aganov <den.aganov@yandex.ru>
2022-06-13 11:58:00 +0300

    Init project

---
+++ QRScanner/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  QRScanner
+//
+//  Created by Denis Aganov on 13.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let controller = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
