3b401fa336bd4c78989e7bd0ddd1f2af53b69870
Denis Aganov <den.aganov@yandex.ru>
2022-06-16 11:50:43 +0300

    Add ProgressView and correct WebView layout

--- QRScanner/WebModule/View/WebViewController.swift
+++ QRScanner/WebModule/View/WebViewController.swift
@@ -3,47 +3,83 @@ import WebKit
 
 class WebViewController: UIViewController, WKUIDelegate {
     
-    var webView: WKWebView!
+    var webView = WKWebView()
+    var progressBar = UIProgressView()
     
     var presenter: WebViewPresenterProtocol!
     weak var delegate: ControlCameraProtocol?
     
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupNavigation()
-
+        setupWebView()
+        setupProgressBar()
+        
         presenter.setUrl()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        webView.frame = view.bounds
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         delegate?.startCamera()
     }
     
-    func setupNavigation() {
+    // MARK: - Setup controls
+    fileprivate func setupNavigation() {
         let buttonDone = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(doneButtonAction))
         let buttonShare = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.action, target: self, action: nil)
-
+        
         navigationItem.leftBarButtonItem = buttonDone
         navigationItem.rightBarButtonItem = buttonShare
+        
+        view.backgroundColor = .systemBackground
     }
     
+    fileprivate func setupWebView() {
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    fileprivate func setupProgressBar() {
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        webView.addSubview(progressBar)
+        
+        progressBar.progress = 0.1
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            progressBar.topAnchor.constraint(equalTo: webView.topAnchor),
+            progressBar.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+            progressBar.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+        ])
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            print("Current progress: \(Int(webView.estimatedProgress * 100))%")
+            if webView.estimatedProgress >= 1.0 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.progressBar.alpha = 0.0
+                }, completion: { _ in
+                    self.progressBar.setProgress(0.0, animated: false)
+                })
+            } else {
+                progressBar.alpha = 1.0
+                progressBar.setProgress(Float(webView.estimatedProgress), animated: true)
+            }
+        }
+    }
 }
 
 extension WebViewController {
     @objc func doneButtonAction(button: UIBarButtonItem) {
-        dismiss(animated: true)        
+        dismiss(animated: true)
     }
 }
 
